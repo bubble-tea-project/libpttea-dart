79cf2ecb6ec2489d00e2246b9a7c55b79cee1191
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-10 02:18:10 +0800

    refactor

--- libpttea/lib/src/websocket_client.dart
+++ libpttea/lib/src/websocket_client.dart
@@ -1,9 +1,17 @@
+/// libpttea.websocket_client
+/// ----------
+///
+/// This module provides the WebSocket client for connecting to PTT.
+library;
+
+import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
+
 import 'package:logging/logging.dart';
-import './utils.dart';
+
+import './utils.dart' as utils;
 
 final Logger logger = Logger('websocket_client');
 final Logger loggerMessages = Logger('websocket_client_messages');
@@ -11,15 +19,41 @@ final Logger loggerMessages = Logger('websocket_client_messages');
 class WebSocketClient {
   final String url;
   final String origin;
+
   WebSocket? _connection;
-  final StreamController<List<int>> _receiveController = StreamController<List<int>>();
-  final AsyncQueue<List<int>> receiveQueue = AsyncQueue<List<int>>();
-  
-  final StreamController<Uint8List> _sendController = StreamController<Uint8List>();
   bool connected = false;
 
-  WebSocketClient({this.url = 'wss://ws.ptt.cc/bbs/', this.origin = 'https://term.ptt.cc'});
+  final utils.AsyncQueue<List<int>> receiveQueue =
+      utils.AsyncQueue<List<int>>();
 
+  WebSocketClient(
+      {this.url = 'wss://ws.ptt.cc/bbs/', this.origin = 'https://term.ptt.cc'});
+
+  /// Handler to receive messages from the WebSocket connection.
+  void _receiveHandler() {
+    _connection?.listen(
+      (message) {
+        // message is binary data (List<int>)
+        receiveQueue.put(message as List<int>);
+
+        if (loggerMessages.level <= Level.INFO) {
+          String decodedMessage = utf8.decode(message, allowMalformed: true);
+          loggerMessages.info('Receive >>$decodedMessage<<\n');
+        }
+      },
+      onDone: () {
+        logger.info('WebSocket ConnectionClosed');
+        connected = false;
+      },
+      onError: (error) {
+        // refactor!
+        logger.severe('Error receiving message: $error');
+        connected = false;
+      },
+    );
+  }
+
+  /// Create connection.
   Future<void> connect() async {
     try {
       _connection = await WebSocket.connect(
@@ -27,73 +61,41 @@ class WebSocketClient {
         headers: {'Origin': origin},
       );
       connected = true;
-      logger.info('Connected to WebSocket at $url');
+      logger.info('Connected');
 
       // Start listening to messages
       _receiveHandler();
-      // Start handling outgoing messages
-      _sendHandler();
     } catch (e) {
+      // refactor!
       logger.severe('Connection failed: $e');
       connected = false;
     }
   }
 
-  void _receiveHandler() {
-    _connection?.listen(
-      (message) {
-        // message is binary data (List<int>)
-        _receiveController.add(message as List<int>);
-        receiveQueue.put(message as List<int>);
-
-        if (loggerMessages.level <= Level.INFO) {
-          String decodedMessage = utf8.decode(message, allowMalformed: true);
-          loggerMessages.info('Receive >>$decodedMessage<<\n');
-        }
-    
-
-      },
-      onDone: () {
-        logger.info('Connection closed by server.');
-        connected = false;
-      },
-      onError: (error) {
-        logger.severe('Error receiving message: $error');
-        connected = false;
-      },
-    );
-  }
-
-  void _sendHandler() {
-    _sendController.stream.listen((message) async {
-      if (connected && _connection != null) {
-        try {
-          _connection?.add(message);
-          loggerMessages.info('Sent >>$message<<');
-        } catch (e) {
-          logger.severe('Error sending message: $e');
-        }
-      } else {
-        logger.warning('Cannot send message, WebSocket is not connected');
-      }
-    });
-  }
-
+  /// Send messages to the WebSocket connection.
   void send(Uint8List message) {
-    _sendController.add(message);
+    if (connected && _connection != null) {
+      try {
+        _connection?.add(message);
+        loggerMessages.info('Sent >>$message<<');
+      } catch (e) {
+        // refactor!
+        logger.severe('Error sending message: $e');
+      }
+    } else {
+      throw Exception("Cannot send message, WebSocket is not connected");
+    }
   }
 
+  /// Close the WebSocket connection.
   Future<void> close() async {
     if (_connection != null) {
       await _connection?.close();
-      _receiveController.close();
-      _sendController.close();
-      logger.info('Connection closed');
+
       connected = false;
+      logger.info('Connection closed manually');
     } else {
       logger.warning('WebSocket is not connected');
     }
   }
-
-  Stream<List<int>> get messages => _receiveController.stream;
 }
