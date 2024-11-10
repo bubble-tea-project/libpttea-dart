6d34b6257a0c4dcf5f194d82bbf68eac83c0658a
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-10 21:58:49 +0800

    Update libpttea_example.dart

--- libpttea/example/libpttea_example.dart
+++ libpttea/example/libpttea_example.dart
@@ -1,6 +1,21 @@
-import 'package:libpttea/libpttea.dart';
+import 'package:libpttea/libpttea.dart' as libpttea;
 
-void main() {
-  // var awesome = Awesome();
-  // print('awesome: ${awesome.isAwesome}');
+const pttAccount = "PTT ID";
+const pttPassword = "PTT 密碼";
+
+Future<void> main() async {
+  final libPttea =
+      await libpttea.login(pttAccount, pttPassword, delDuplicate: true);
+
+  final systemInfo = await libPttea.getSystemInfo();
+  print(systemInfo);
+  // [您現在位於 批踢踢實業坊 (140.112.172.11),
+  // 系統負載: 輕輕鬆鬆,
+  // 線上人數: 59384/175000,
+  // ClientCode: 02000023,
+  // 起始時間: 11/10/2024 05:17:51,
+  // 編譯時間: Sun Jun  4 23:41:30 CST 2023,
+  // 編譯版本: https://github.com/ptt/pttbbs.git 0447b25c 8595c8b4 M]
+
+  await libPttea.logout();
 }
